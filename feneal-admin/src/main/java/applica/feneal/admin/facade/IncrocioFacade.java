59ef2827ea1ab42a05946dce9bf7318a0d72cc61
felicegramegna <felice.99gramegna@gmail.com>
2020-12-16 13:11:21 +0100

    modifiche data format

--- feneal-admin/src/main/java/applica/feneal/admin/facade/IncrocioFacade.java
+++ feneal-admin/src/main/java/applica/feneal/admin/facade/IncrocioFacade.java
@@ -5,9 +5,13 @@ import applica.feneal.domain.model.dbnazionale.LavoratoreIncrocioNoniscritti;
 import applica.feneal.domain.model.dbnazionale.search.ReportIncrocioNonIscrittiSearchParams;
 import applica.feneal.domain.model.dbnazionale.search.ReportIncrocioSearchParams;
 import applica.feneal.services.ReportIncrocioService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -17,16 +21,28 @@ public class IncrocioFacade {
     private ReportIncrocioService reportIncrocioService;
 
 
-    public List<LavoratoreIncrocio> retrieveLavoratoriIncrocio(ReportIncrocioSearchParams params) {
+    public List<LavoratoreIncrocio> retrieveLavoratoriIncrocio(ReportIncrocioSearchParams params) throws ParseException {
         List<LavoratoreIncrocio> result;
+
+        if(!StringUtils.isEmpty(params.getData())){
+            Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(params.getData());
+            SimpleDateFormat g = new SimpleDateFormat("yyyy-MM-dd");
+            params.setData(g.format(date1));
+        }
 
         result = reportIncrocioService.retrieveIncrocioProvinciaResidenza(params);
 
         return result;
     }
 
-    public List<LavoratoreIncrocioNoniscritti> retrieveLavoratoriIncrocioNonIscritti(ReportIncrocioNonIscrittiSearchParams params) {
+    public List<LavoratoreIncrocioNoniscritti> retrieveLavoratoriIncrocioNonIscritti(ReportIncrocioNonIscrittiSearchParams params) throws ParseException {
         List<LavoratoreIncrocioNoniscritti> result;
+
+        if(!StringUtils.isEmpty(params.getData())){
+            Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(params.getData());
+            SimpleDateFormat g = new SimpleDateFormat("yyyy-MM-dd");
+            params.setData(g.format(date1));
+        }
 
         result = reportIncrocioService.retrieveIncrocioNoniscritti(params);
 
